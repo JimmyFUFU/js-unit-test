bda06c275709801bf2b537275cfddcd557e1bc2a
JimmyFUFU <ppp456rrr@gmail.com>
2021-04-22 00:01:36 +0800

    temp

--- test/authentication.test.js
+++ test/authentication.test.js
@@ -1,17 +1,16 @@
 import {Authentication} from "../src/authentication";
-import * as otp from "../src/otp";
+import * as otp from '../src/otp'
 
-jest.mock("../src/otp");
+// jest.mock("../src/authentication")
+jest.mock('../src/otp')
 
 describe('authenticate account is valid', function () {
     it('should be valid', () => {
         let authentication = new Authentication();
         const fakeGetPassword = jest.fn();
+        fakeGetPassword.mockReturnValueOnce('91')
         authentication.getPassword = fakeGetPassword;
-        fakeGetPassword.mockReturnValueOnce('91');
-
         otp.get_token.mockReturnValueOnce('000000');
-
         expect(authentication.is_valid('joey', '91000000')).toBe(true);
     });
 });
